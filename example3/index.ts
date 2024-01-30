073d7b504924e30a40c53a3fdbdd9d5d380dc96b
weihan <weihan@artafinance.com>
2024-01-30 16:31:11 +0800

    repro

--- example3/index.ts
+++ example3/index.ts
@@ -7,4 +7,6 @@ const testTopic1 = new gcp.pubsub.Topic("test-topic1", {});
 
 const testTopic2 = new gcp.pubsub.Topic("test-topic2", {});
 
+const testTopic3 = new gcp.pubsub.Topic("test-topic3", {}, { dependsOn: [testTopic1] });
+
 
