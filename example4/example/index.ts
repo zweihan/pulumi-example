6e475b1fbd9639bddb56c40bb7b4bfbec96e2975
weihan <zhengweihan.91@gmail.com>
2024-09-12 15:37:43 +0800

    example4

---
+++ example4/example/index.ts
@@ -0,0 +1,18 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+
+
+const pubsub1 = new gcp.pubsub.Topic("test-topic1", {name: 'test-topic1'});
+const pubsub2 = new gcp.pubsub.Topic("test-topic2", {name: 'test-topic2'});
+
+
+const sub1 = new gcp.pubsub.Subscription('test-sub1', {
+  name: 'test-sub1',
+  topic: pubsub1.name,
+});
+
+const sub2 = new gcp.pubsub.Subscription('test-sub2', {
+  name: 'test-sub2',
+  topic: pubsub2.name,
+});
